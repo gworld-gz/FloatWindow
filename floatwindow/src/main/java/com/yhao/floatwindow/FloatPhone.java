32507ab88682a3711ea365f5a237fe8095b8bad7
Chee <205877309@qq.com>
2019-12-10 15:35:09 +0800

    权限申请成功回调处理

--- floatwindow/src/main/java/com/yhao/floatwindow/FloatPhone.java
+++ floatwindow/src/main/java/com/yhao/floatwindow/FloatPhone.java
@@ -83,6 +83,9 @@ class FloatPhone extends FloatView {
             try {
                 mLayoutParams.type = WindowManager.LayoutParams.TYPE_TOAST;
                 mWindowManager.addView(mView, mLayoutParams);
+                if (mPermissionListener != null) {
+                    mPermissionListener.onSuccess();
+                }
             } catch (Exception e) {
                 mWindowManager.removeView(mView);
                 LogUtil.e("TYPE_TOAST 失败");
